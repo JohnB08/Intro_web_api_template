3c30b32a05932ae21e79d1c20c1c4b11088982c8
johnb08 <johnb08.kodehode@gmail.com>
2025-04-02 14:22:58 +0200

    mappet model to endpoints

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 /* Her lager vi en referanse til et builder objekt, som kan samle alle konfigurasjonsobjekter samt en ting som heter depencency containeren under en fellesreferanse, slik at vi kan fylle de ut før vi faktisk "bygger" appen vår.
 Måten dette fungerer på er via en designpattern som heter builder prinsippet, hvor du stykker opp constructoren av et objekt i flere byggesteg. */
+using Intro_web_api_template.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 Mangen av disse servicene er tilgjengelige via det som heter dependency injection. Vi skal kikke på hvordan dette ser ut senere når vi introduserer Controllers inn i applikasjonen vår.*/
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/* Her legger vi til en service som skal representere vår TaskContext. 
+Singleton betyr at det bare skal eksistere en "lifetime" av dette objektet, så lenge appen vår kjøres.
+Aka hver gang vi refererer til en TaskContext i en metode i appen vår, refererer vi til samme instans av TaskContext.
+
+Vi legger en instans av TaskContext inn i vår Dependency Container.   */
+builder.Services.AddSingleton<TaskContext>();
 
 var app = builder.Build();
 
@@ -29,8 +39,46 @@ app.MapGet("/", ()=>"Hello, gang!");
 /* Vi har nå bedd vår app, om å lytte etter get requests til default routen sin (tenk prefixUrl fra forrige uke). Når den får en get request til den routen, skal den returne teksten Hello, gang! */
 
 /* La oss sette opp modellene fra TaskManager appen vår fra to uker siden. */
+/* Vi kan tilgjengeliggjøre de samme modellene i vår api. se linje 20 */
+
+/* Legg merke til at lytterene nedenfor "lytter" på en tillegsbit til prefixUrlen vår. 
+Det kan være lurt at denne "suffixen" er en god, og berskrivende endepunkt som forklarer tydlig til brukeren vår hva resurs de jobber mot.*/
+
+/* La oss lage et endepunkt som kan legge til en Task. Legg merke til vi bruker http metoden POST
+som er standardmetoden som referer til at en klient prøver å "poste" en ny resurs til vårt endepunkt. */
+
+app.MapPost("/taskmanager", (string title, string description, DateTime dueDate, TaskContext context ) => context.AddTask(title, description, dueDate));
+
+/* La oss lage et endepunkt som kan gette alle tasks. */
+
+app.MapGet("/taskmanager", (TaskContext context) => context.GetAllTasks());
+
+/* Vi lager endepunkter for å hente pending og complete */
+
+app.MapGet("/taskmanager/complete", (TaskContext context) => context.GetCompleteTasks());
+
+app.MapGet("/taskmanager/pending", (TaskContext context) => context.GetPendingTasks());
+
+/* Vi kan lage endepunkter som kan fetche enkelt tasks */
+/* Legg merke til at vi kan hente ut id fra route via {id}, husk tilbake til hvordan rawurl kunne brukes til å lese routen vår bak prefixUrlen vår.  */
+app.MapGet("/taskmanager/{id}", (int id, TaskContext context) => context.GetTaskById(id));
 
 
+/* Hvis en klient ønsker å endre en resurs vi tilgjengeliggjør, bruker de gjerne metoden Patch. Siden vi her ønsker å complete en task via en id, kan vi appende en {id} til vår taskmanager/complete route. */
+app.MapPatch("/taskmanager/complete/{id}", (int id, TaskContext context) => context.CompleteTask(id));
+
+/* Det samme hvis vi skal slette en resurs, når en klient ønsker å slette en resurs vi tilgjengeliggjør, så bruker de gjerne http metoden DELETE */
+app.MapDelete("/taskmanager/{id}", (int id, TaskContext context) => context.DeleteTask(id));
+
+
+/* For å teste appen vår på dette tidspunktet kan det være greit å bruke run daemonen tilgjengelig for oss i dotnet dev tools.
+Daemonen lar oss lytte etter filendringer, og softrestarte programmet vårt. Vi slipper å stenge og starte applikasjonen vår på nytt hver gang vi gjør endringer.
+Den er tilgjengelig via dotnet watch
+
+for eksempel for å kjøre denne applikasjonen via watch bruker vi 
+
+dotnet watch run
+ */
 /* Dette er middleware for å redirekte http requests til https request, om de korekte sertifikatene er satt opp. */
 app.UseHttpsRedirection();
 /* Her kjører vi applikasjonen vår. Fungerer veldig likt hvordan vi kjørte en webserver i forrige uke.
